808fb1f1de2573fcf3896888ced0c0a1cc815c75
Jifeng Zhang <jifeng.zhang@svenskaspel.se>
2016-09-23 12:16:54 +0200

    scripts for different environment

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-docker_repo=944159926332.dkr.ecr.eu-west-1.amazonaws.com/event-based-cd-example:latest
-docker stop event-based-cd-example
-docker rm event-based-cd-example
-docker run -d -p 8080:5000 --name event-based-cd-example ${docker_repo}
--- start_local.sh
+++ start_local.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 docker_repo=diabol/event-based-cd-example:latest
-docker stop event-based-cd-example
-docker rm event-based-cd-example
-docker run -d -p 8080:5000 --name event-based-cd-example ${docker_repo}
+container_name=event-based-cd-example-local
+docker stop ${container_name}
+docker rm ${container_name}
+docker run -d -p 8080:5000 --name ${container_name} ${docker_repo}
