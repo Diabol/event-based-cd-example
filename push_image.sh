93e5183eed70a8c0263439735edeafda206f74a0
Jifeng Zhang <jifeng.zhang@svenskaspel.se>
2016-09-23 15:13:13 +0200

    separate build and push image scripts

--- push_image.sh
+++ push_image.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -x
 docker_repo=944159926332.dkr.ecr.eu-west-1.amazonaws.com/event-based-cd-example:latest
+docker tag diabol/event-based-cd-example:latest ${docker_repo}
 docker push ${docker_repo}
