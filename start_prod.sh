1ab737db3f793de4e41cee0d080683648a962419
Jifeng Zhang <jifeng.zhang@svenskaspel.se>
2016-09-23 15:05:57 +0200

    remove old scripts

--- start_prod.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-docker_repo=944159926332.dkr.ecr.eu-west-1.amazonaws.com/event-based-cd-example:latest
-container_name=event-based-cd-example-prod
-docker stop ${container_name}
-docker rm ${container_name}
-docker run -d -p 80:5000 --name ${container_name} ${docker_repo}
