3311bf8baf1354a44db6572444bbdbec8f82a932
Jifeng Zhang <jifeng.zhang@svenskaspel.se>
2016-09-23 12:08:23 +0200

    run container from ecr repo

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+set -x
+docker_repo=944159926332.dkr.ecr.eu-west-1.amazonaws.com/event-based-cd-example:latest
 docker stop event-based-cd-example
 docker rm event-based-cd-example
-docker run -d -p 8080:5000 --name event-based-cd-example diabol/event-based-cd-example:latest
+docker run -d -p 8080:5000 --name event-based-cd-example ${docker_repo}
