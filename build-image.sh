ec4d556eb0a29acdc28cd84ba1a9d010b256bfe4
Jifeng Zhang <jifeng.zhang@svenskaspel.se>
2016-09-23 11:29:49 +0200

    push to aws ecr

--- build-image.sh
+++ build-image.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-docker build -f Dockerfile . -t diabol/event-based-cd-example:latest
+set -x
+docker_repo=944159926332.dkr.ecr.eu-west-1.amazonaws.com/event-based-cd-example:latest
+docker build -f Dockerfile -t diabol/event-based-cd-example:latest .
+docker tag diabol/event-based-cd-example:latest ${docker_repo}
+docker push ${docker_repo}
