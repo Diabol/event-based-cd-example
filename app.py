0fba245b2280b2de71423aea6d75ae0b805f75ca
Jifeng Zhang <zjfroot@gmail.com>
2016-09-24 10:45:20 +0200

    text update

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def hello():
     #cat_url = 'https://github.com/Diabol/event-based-cd-example/blob/master/cat2.gif?raw=true'
     #reply = '<body background=\'http://2.bp.blogspot.com/-TwuKgh-bJO8/T9EfubfSyZI/AAAAAAAAC2I/FSaTSfqrXhk/s400/cat-playing-guitar-gif.gif\'>'
     reply = '<h1>!!!Deployed by the Super Cool Event Based CD System!!!</h1>\n'
-    reply += '<h2>Environment: '+provider +' deployed at ' + deployed_time + '</h2>'
+    reply += '<h2>Environment: **'+provider +'** deployed at: ' + deployed_time + '</h2>'
     reply += '<image src=\'' + cat_url + '\' height=\'444\' width=\'560\'/>\n'
     #reply += '</body>'
     return reply
